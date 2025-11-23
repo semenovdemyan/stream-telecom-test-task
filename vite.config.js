d3ed344f96123d26ef6203322c7ee9d211f4b4a3
Demian Semenov <semenovdemyan@gmail.com>
2025-11-23 16:46:10 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,17 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// __dirname аналог для ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// получаем абсолютный путь к корню проекта без process
-const projectRoot = path.resolve(__dirname);
-
-export default defineConfig(({ mode }) => {
-  // загружаем .env или .env.production
-  const env = loadEnv(mode, projectRoot, '');
-
-  return {
-    plugins: [react()],
-    base: env.VITE_BASE_URL || '/',
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
-      },
+export default defineConfig({
+  plugins: [react()],
+  base: '/stream-telecom-test-task/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-  };
+  },
 });
